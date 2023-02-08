3736654e605edfd2be14e0d0e66288054a85d616
Nataliya <nataliya.solina@bsc-ideas.com>
2023-02-08 23:54:47 +0300

    Change generator

--- src/main/java/org/example/user/UserGen.java
+++ src/main/java/org/example/user/UserGen.java
@@ -5,8 +5,11 @@ import org.apache.commons.lang3.RandomStringUtils;
 import java.util.Random;
 
 public class UserGen {
+    public static final String EMAIL = "Tasha261280@ya.ru";
+    public static final String PASSWORD = "2611";
+    public static final String NAME = "Tasha";
     public User def() {
-        return new User("Tasha261280@ya.ru", "2611", "Tasha");
+        return new User(EMAIL, PASSWORD, NAME);
     }
 
     public User random() {
